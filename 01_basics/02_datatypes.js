cb8071961bf87c56b49f69aee7eef2356a79475a
Abhinav Aditya <abhinav69967@gmail.com>
2024-01-22 18:36:30 +0530

    datatype discussion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as per new version
+
+//alert(3+3)  //This will give error as we are using node js not browser
+
+console.log(3+3) //code readability shoud be high
+
+let name = "Abhinav"
+let age = 22
+let isLoggedIn = true
+
+
+// number => 2 to the power 50
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+// object
+
+console.log(typeof name)
+console.log(typeof age)
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null) //object
+
